b440a039ce479e30b28648ddf12c7fdc00b6d918
Akulov Kac <akulovkac@gmail.com>
2018-03-22 13:30:42 +0200

    first commit

---
+++ WindowsFormsDz_3/WindowsFormsDz_3/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsDz_3
+{
+    //Задание 1
+    //Разработайте программу, которая позволяет пользователю осуществлять поиск файлов по заданному критерию.
+    //Приложение состоит из двух форм – главное окно и окно поиска.Пользователь может открыть сколько угодно окон для поиска.
+    //Окно поиска запускается немодально. В окне поиска пользователь выбирает папку, в которой будет идти поиск 
+    //и вводит маску поиска, например «*.doc». Найденные файлы отображаются в списке.
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ WindowsFormsDz_3/WindowsFormsDz_3_3/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsDz_3_3
+{
+    //Задание 3
+    //Разработайте приложение, которое состоит из двух форм.Первая форма содержит TextBox доступный только для 
+    //чтения и две кнопки «загрузить файл» и «редактировать». Кнопка «редактировать» изначально неактивна. 
+    //При нажатии на первую кнопку, открывается диалог и пользователю предлагают выбрать текстовый файл.
+    //Выбранный файл загружается в TextBox и кнопка «редактировать» становится активной.
+    //При нажатии на вторую кнопку открывается вторая форма (не модально), которая содержит TextBox 
+    //доступный для редактирования и две кнопки «Сохранить» и «Отменить». При нажатии на первую кнопку 
+    //изменения отображаются в TextBox первой формы.
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
